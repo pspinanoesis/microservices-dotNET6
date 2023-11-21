49f1d40017c777af0a044688f6dd1adfe8704124
Paulo Pina <paulo.s.pina@noesis.pt>
2023-11-21 15:10:46 +0000

    DataBase configured

---
+++ GeekShooping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySQLContext :DbContext
+    {
+        
+        public MySQLContext() { } //default constructor
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
--- GeekShooping/GeekShopping.ProductAPI/Program.cs
+++ GeekShooping/GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,7 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace GeekShopping.ProductAPI
 {
     public class Program
@@ -6,12 +10,18 @@ namespace GeekShopping.ProductAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
             // Add services to the container.
             builder.Services.AddAuthorization();
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<MySQLContext>(options => options.
+                                                            UseMySql(connection, 
+                                                                     new MySqlServerVersion(
+                                                                           new Version(8, 0, 12))));
 
             var app = builder.Build();
 
