595e7a2a8062d9619dcf6d612c1fff307b5feff4
Paulo Pina <paulo.s.pina@noesis.pt>
2024-01-15 16:20:39 +0000

    Implementado o Cadastro de novos Usarios

--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -33,7 +33,7 @@ namespace GeekShopping.ProductAPI
             builder.Services.AddControllers();
 
             builder.Services.AddAuthentication("Bearer")
-                .AddJwtBearer(options => {
+                .AddJwtBearer("Bearer", options => {
                     options.Authority = "https://localhost:4435";
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
