734169813280cc9ee66e42bb035b3bbaa004f031
Paulo Pina <paulo.s.pina@noesis.pt>
2024-07-23 17:18:32 +0100

    implemented the Interface IPersonService, part 2

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,61 @@
+﻿using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i > 8; i++) 
+            { 
+                Person person = MockPerson(i);
+                people.Add(person);
+            }
+            return people;
+        }
+
+        
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "FName",
+                LastName = "LName",
+                Address = "Address",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = 1,
+                FirstName = "FName",
+                LastName = "LName",
+                Address = "Address",
+                Gender = "Male"
+            };
+        }
+    }
+}
