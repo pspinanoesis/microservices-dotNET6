fcd7f0a587eaa5637bbcbad75ef679aac6018b59
Paulo Pina <paulo.s.pina@noesis.pt>
2024-07-23 13:08:41 +0100

    Criado projeto RestWithASPNETUdemy definido a Estrutura Basica da calculadora

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatortController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatortController : ControllerBase
+    {
+        
+
+        private readonly ILogger<CalculatortController> _logger;
+
+        public CalculatortController(ILogger<CalculatortController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sUM/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private int ConvertToDecimal(string number)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsNumeric(string number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
