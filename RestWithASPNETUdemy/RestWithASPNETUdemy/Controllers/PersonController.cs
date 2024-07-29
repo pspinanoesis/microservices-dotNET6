86c1f86b0a0604f2ba50d511d3032d08f6988346
Paulo Pina <paulo.s.pina@noesis.pt>
2024-07-29 12:35:27 +0100

    Ajusting Controller

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -1,12 +1,73 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Model;
 
 namespace RestWithASPNETUdemy.Controllers
 {
-    public class PersonController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
     {
-        public IActionResult Index()
+
+        private readonly ILogger<CalculatorController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<CalculatorController> logger, IPersonService personService)
         {
-            return View();
+            _logger = logger;
+            _personService = personService;
         }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            return Ok(_personService.FindAll());
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+        
+        [HttpPost("{id}")]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_personService.Create(person));
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(long id)
+        {
+             _personService.Delete(id);
+            return NoContent();
+        }
+
     }
 }
--- RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -1,11 +1,14 @@
 ﻿using RestWithASPNETUdemy.Model;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace RestWithASPNETUdemy.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
+        private volatile int count;
+
         public Person Create(Person person)
         {
             return person;
@@ -50,12 +53,17 @@ namespace RestWithASPNETUdemy.Services.Implementations
         {
             return new Person
             {
-                Id = 1,
-                FirstName = "FName",
-                LastName = "LName",
-                Address = "Address",
+                Id = IncrementAndGet(),
+                FirstName = "FName " + i,
+                LastName = "LName " + i,
+                Address = "Some Address" + i,
                 Gender = "Male"
             };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
         }
     }
 }
