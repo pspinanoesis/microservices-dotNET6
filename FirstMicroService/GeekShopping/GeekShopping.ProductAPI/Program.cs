92a8b913e1f4f54f320523d3ad124ef44415ca35
Paulo Pina <paulo.s.pina@noesis.pt>
2024-07-30 10:55:41 +0100

    created the database and configured acess from the microservice ProductAPI to the database

--- FirstMicroService/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ FirstMicroService/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace GeekShopping.ProductAPI
 {
     public class Program
@@ -6,8 +9,15 @@ namespace GeekShopping.ProductAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
 
+            var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+            builder.Services.AddDbContext<MySQLContext>(options => options
+                .UseMySql(connection
+                    , new MySqlServerVersion(
+                        new Version(8, 0, 12))));
+
+            // Add services to the container.
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
